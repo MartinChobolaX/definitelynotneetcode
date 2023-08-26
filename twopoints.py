4b2160f3d3fa689ea0f4ddf0af0d84a80e560ac8
MartinChobolaX <53659720+MartinChobolaX@users.noreply.github.com>
2023-08-26 16:36:17 +0200

    Update twopoints.py

--- twopoints.py
+++ twopoints.py
@@ -303,3 +303,43 @@ class Solution(object):
                 l += 1
         return maxim
 
+
+
+
+
+""""
+42. Trapping Rain Water
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+""""
+
+def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        leftMax, rightMax = 0, 0
+        waterTrapped = 0
+
+        while left < right:
+            print(left,right)
+            if height[left] <= height[right]:
+                leftMax = max(leftMax, height[left])
+                waterTrapped += leftMax - height[left]
+                left += 1
+            else:
+                rightMax = max(rightMax, height[right])
+                waterTrapped += rightMax - height[right]
+                right -= 1
+
+        return waterTrapped
